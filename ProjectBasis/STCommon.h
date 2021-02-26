143161f2727f107539774a5e429ded14ef2536ca
wangtao-iOS <411433584@qq.com>
2021-02-26 13:58:23 +0800

    创建

---
+++ ProjectBasis/STCommon.h
@@ -0,0 +1,13 @@
+//
+//  STCommon.h
+//  studyTom
+//
+//  Created by Victor on 2018/7/16.
+//  Copyright © 2018年 Victor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STCommon : NSObject
+
+@end
---
+++ ProjectBasis/STCommon.m
@@ -0,0 +1,14 @@
+//
+//  STCommon.m
+//  studyTom
+//
+//  Created by Victor on 2018/7/16.
+//  Copyright © 2018年 Victor. All rights reserved.
+//
+
+#import "STCommon.h"
+
+@implementation STCommon
+
+
+@end
